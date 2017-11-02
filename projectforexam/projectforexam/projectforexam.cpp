24190a839fc1eadb65794b01027ea289b64c17dd
wojciechbebenek <wojciech.bebenek@outlook.com>
2017-11-02 14:30:31 +0100

    Nothing really changed

--- projectforexam/projectforexam/projectforexam.cpp
+++ projectforexam/projectforexam/projectforexam.cpp
@@ -6,7 +6,7 @@
 #include <fstream>
 #include <string>
 #include <cctype>
-
+void CheckFilename(std::string filename);
 int main()
 {
 	using namespace std;
@@ -15,37 +15,54 @@ int main()
 
 	int errorcode = 0;
 
-	string errors[11];
-	errors[1] = "Invalid characters in file name";
-	errors[2] = "I said, without ";
+	
+
 	cout << "Give me name of the file (without extension buddy) that is in program's folder" << endl;
 	getline(cin, filename); //gettin' da line
+
+	
+	CheckFilename(filename);
+
+    return 0;
+}
+
+void CheckFilename(std::string filename) {
+
+	std::string errors[11];
+	errors[1] = "Invalid characters in file name";
+	int errorcode = 0;
+
 	for (int i = 0; i < filename.length();i++) {
-	char actualchar = filename[i]; // convertin' to char
-	int v = actualchar; // gettin ascii value of character
-	if (isalnum(filename[i])) {
-	//cout << filename[i];
-	}
-	else if(v == 47 || v == 92 || v == 124 || v == 58 || v == 42 || v == 34 || v == 63 || v == 60 || v == 62){// forbidden asciis: 47,92,124, 58, 42,34,63,60,62
-		errorcode = 1;
-		break;
-	}
+		char actualchar = filename[i]; // convertin' to char
+		int v = actualchar; // gettin ascii value of character
+		if (isalnum(filename[i])) {
+			//cout << filename[i];
+		}
+		else if (v == 47 || v == 92 || v == 124 || v == 58 || v == 42 || v == 34 || v == 63 || v == 60 || v == 62) {// forbidden asciis: 47,92,124, 58, 42,34,63,60,62
+			errorcode = 1;
+			break;
+		}
 
 
 	}
 
 	if (errorcode == 0) {
-		cout << endl << "cool name";
+		std::cout << std::endl << "cool name";
 	}
 	else {
 		for (int i = 1; i < 11; i++) {
 			if (errorcode == i) {
-				cout << endl << errors[i];
+				std::cout << std::endl << errors[i];
 			}
 		}
 
 	}
-
-    return 0;
 }
 
+/*
+32-126 asciis with chars
+
+Asciis a-z
+65 97
+90 122
+*/
